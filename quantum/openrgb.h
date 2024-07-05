0d1ff2a6df52d0c90c8155311b1f4ce838e62929
Naitoshedo <hozuki@naitoshedo.tv>
2024-07-05 18:25:08 -0500

    Update openrgb.h

--- quantum/openrgb.h
+++ quantum/openrgb.h
@@ -20,7 +20,7 @@
 // so OpenRGB can detect compatible firmware.
 #define OPENRGB_PROTOCOL_VERSION 0xD
 
-#define RAW_EPSIZE 64
+#define RAW_EPSIZE 32
 
 enum openrgb_command_id {
     OPENRGB_GET_PROTOCOL_VERSION = 1,
